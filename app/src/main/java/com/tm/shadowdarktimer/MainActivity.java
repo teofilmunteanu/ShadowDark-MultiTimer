f8c1f9534d1eb3b527072b2d47ccc364eb8a254c
Munteanu Teofil <andreiteofil01@gmail.com>
2023-12-04 03:08:45 +0200

    layout + colors

---
+++ app/src/main/java/com/tm/shadowdarktimer/MainActivity.java
@@ -0,0 +1,19 @@
+package com.tm.shadowdarktimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onAddTorchClicked(View view) {
+
+    }
+}
