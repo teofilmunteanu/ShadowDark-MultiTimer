1331922634c442f00c4b3608a59fcc4498623f02
Munteanu Teofil <andreiteofil01@gmail.com>
2024-01-10 20:00:15 +0200

    Global time operations

--- app/src/main/java/com/tm/shadowdarktimer/MainActivity.java
+++ app/src/main/java/com/tm/shadowdarktimer/MainActivity.java
@@ -6,18 +6,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.tm.shadowdarktimer.adapters.TorchAdapter;
 import com.tm.shadowdarktimer.models.TorchModel;
 import com.tm.shadowdarktimer.services.TimerManager;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<TorchModel> torchList = new ArrayList<>();
     TorchAdapter torchAdapter;
+    boolean globalPaused = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +37,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(torchAdapter);
     }
 
-    public void onAddTorchClicked(View view){
-        torchList.add(new TorchModel());
-        torchAdapter.notifyItemInserted(torchList.size()-1);
-    }
-
     @Override
     protected void onStart() {
         super.onStart();
@@ -47,5 +47,53 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
+    }
+
+    public void onAddTorchClicked(View view){
+        torchList.add(new TorchModel());
+        torchAdapter.notifyItemInserted(torchList.size()-1);
+    }
+
+    public void onGlobalPauseClicked(View view){
+        globalPaused = !globalPaused;
+        String playLabel = getString(R.string.play_label);
+        String pauseLabel = getString(R.string.pause_label);
+        ((Button)view).setText(globalPaused ? playLabel : pauseLabel);
+
+        for(TorchModel torch : torchList){
+            if (torch.getPaused() != globalPaused){
+                torch.pauseUnpause();
+            }
+        }
+    }
+
+    public void onGlobalForwardClicked(View view){
+        String globalChangeString = ((EditText)findViewById(R.id.globalTimeChangeInput)).getText().toString();
+
+        for (TorchModel torch : torchList){
+            if (!torch.getPaused()){
+                if(TorchModel.isValidTimeChangeString(globalChangeString)){
+                    torch.fastForward();
+                }
+                else{
+                    torch.resetTimeChange();
+                }
+            }
+        }
+    }
+
+    public void onGlobalBackwardClicked(View view){
+        String globalChangeString = ((EditText)findViewById(R.id.globalTimeChangeInput)).getText().toString();
+
+        for (TorchModel torch : torchList){
+            if (!torch.getPaused()){
+                if(TorchModel.isValidTimeChangeString(globalChangeString)){
+                    torch.fastBackward();
+                }
+                else{
+                    torch.resetTimeChange();
+                }
+            }
+        }
     }
 }
